ed808e1ab36571581e8c6a5210e059647475fd15
antonturch <antonturch@mail.ru>
2022-03-28 17:40:33 +0400

    Add migrations and seeds

---
+++ migrations/20220328113559-create-products.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Products', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      price: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      currency: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      img: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Products');
+  }
+};
