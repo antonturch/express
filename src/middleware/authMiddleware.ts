22afd1c794df69c69aebfd0f76197f7e1abb49d7
antonturch <antonturch@mail.ru>
2022-07-01 13:47:42 +0300

    update auth middleware

--- src/middleware/authMiddleware.ts
+++ src/middleware/authMiddleware.ts
@@ -1,5 +1,4 @@
 import { NextFunction, Request, Response } from "express";
-import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 import { ParamsDictionary } from "express-serve-static-core";
 import tokenService from "../services/tokenService";
@@ -22,25 +21,16 @@ export default function authMiddleware(
   }
   try {
     const accessToken = req.headers.authorization?.split(" ")[1];
-    const googleToken = req.cookies["jwt"];
+    console.log(accessToken);
     if (accessToken && accessToken !== "null") {
       const userData = tokenService.validateAccessToken(accessToken);
-      req.body.user = userData as IUser;
+
       if (!userData) {
         return next(
           new UnauthorizedError({ message: `User doesn't authorized` })
         );
       }
-    } else if (googleToken) {
-      const userFromGoogle = jwt.verify(
-        googleToken,
-        process.env.JWT_ACCESS_SECRET as string
-      );
-      if (!userFromGoogle) {
-        return next(
-          new UnauthorizedError({ message: `User doesn't authorized` })
-        );
-      }
+      req.body.user = userData as IUser;
     } else if (accessToken === "null") {
       return next(
         new UnauthorizedError({ message: `User doesn't authorized` })
